b828f1e597c9022d62d97f1dfc1cc4ccd39782da
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-11 11:23:55 +0300

    removed homeComponent

--- src/components/Maestro.component.tsx
+++ src/components/Maestro.component.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import SplashMaestro from './SplashMaestro.component';
 import MaestroPricing from './MaestroPricing.component';
 import MasonaryGridCTA from './maestro-product-components/maestro-product-masonary-grid.component';
-import HomeContactForm from './HomeContactForm.component';
 import MaestroContactForm from './MaestroContactForm.component';
 import MaestroDetails from './MaestroDetails.component';
 const Maestro: React.FC = () => {
