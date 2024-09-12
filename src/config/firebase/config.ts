dd475b8e332f0cbcaf41e2e83a179d397564008d
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-12 21:07:49 +0300

    correcting  ENV settings

--- src/config/firebase/config.ts
+++ src/config/firebase/config.ts
@@ -10,14 +10,19 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+console.log(import.meta.env);
 
-console.log('Firebase config:', import.meta.env);
 
+console.log('all env', import.meta.env);
+console.log('one key', import.meta.env.VITE_FIREBASE_API_KEY);
+
+// const db = getFirestore(app,'my_collection')
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(app, 'contacts');
 
 
 
