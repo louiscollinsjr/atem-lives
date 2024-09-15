1d625421a996d483990034220eecafb453d3d044
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-15 06:22:49 +0300

    removed bunny  cdn for produciton from index and manifest config

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
   // Set base URL depending on mode
-  base: mode === 'production' ? 'https://atem-lives.b-cdn.net/' : '/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-    },
-  },
-  define: {
-    'process.env': process.env
-  }
+  //base: mode === 'production' ? 'https://atem-lives.b-cdn.net/' : '/',
+  // build: {
+  //   outDir: 'dist',
+  //   rollupOptions: {
+  //     output: {
+  //       assetFileNames: 'assets/[name].[hash].[ext]',
+  //     },
+  //   },
+  // },
+  // define: {
+  //   'process.env': process.env
+  // }
 }));
