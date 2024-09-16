edd8ef98ba5c80a6426d0e57e063926144cbd855
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-16 22:29:47 +0300

    updaed satchel

--- src/components/SplashSatchel.component.tsx
+++ src/components/SplashSatchel.component.tsx
@@ -5,19 +5,19 @@ const SplashSatchel: React.FC = () => {
     <section className="bg-gray-100 items-center justify-center">
       <div className='container flex flex-col items-center justify-center h-screen transition-all duration-800 mx-auto'>
         <div className="flex flex-col items-center justify-center p-12">
-          <h1 className="~text-[4rem]/[10rem] font-medium font-spartan text-gray-900 items-center justify-center text-center uppercase tracking-widest">
+          <h1 className="~text-4xl/7xl font-medium font-spartan text-gray-900 items-center justify-center text-center uppercase tracking-widest pb-8">
      S❤️tchel.
           </h1>
-          <p className="~text-xs/sm tracking-wide justify-center font-space text-center">
-          At the core of our business lies a passion for crafting designs that inspire and ignite emotions in our clients and their customers. 
+          <p className="~text-xs/sm tracking-wide justify-center font-space text-center max-w-[400px]">
+          At the core of our business lies a passion for creating immersive experiences that captivate and evoke emotions, transforming the way clients and their customers engage with the world. Coming Soon... 
           </p> 
           <p className="justify-center pt-20">
-          <a
+          {/* <a
             href="https://calendly.com/louiscollinsjr/atem-intro"
             className="bg-black text-white text-xs px-3 py-1.5 rounded-full"
           >
             Learn more
-          </a>
+          </a> */}
         </p>
         </div>
        
