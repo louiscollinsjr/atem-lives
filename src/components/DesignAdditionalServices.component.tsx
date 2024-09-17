f1b4eaf387d988442904a7a095f145dc0b378f5a
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-17 14:55:54 +0300

    updated stying

--- src/components/DesignAdditionalServices.component.tsx
+++ src/components/DesignAdditionalServices.component.tsx
@@ -14,22 +14,21 @@ const DesignAdditionalServices: React.FC = () => {
   ];
 
   return (
-    <section className="w-full px-4 py-12 text-left bg-slate-100">
-    <div className="max-w-6xl mx-auto py-24 text-left">
-      <h1 className="text-4xl font-bold mb-4 px-8">Additional Services</h1>
-      <p className="text-lg mb-8 px-8">We offer a range of additional services to enhance your website and business.</p>
-      
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
-        {services.map((service, index) => (
-          <div key={index} className="">
-            <h2 className="text-xl font-semibold mb-2">{service.title}</h2>
-            <p className="text-gray-600 md:text-sm text-base md:w-3/4">{service.description}</p>
-          </div>
-        ))}
+    <section className="w-full bg-slate-100">
+      <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl sm:text-4xl font-bold mb-4">Additional Services</h1>
+        <p className="text-lg mb-8">We offer a range of additional services to enhance your website and business.</p>
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
+          {services.map((service, index) => (
+            <div key={index} className="mb-6">
+              <h2 className="text-xl font-semibold mb-2">{service.title}</h2>
+              <p className="text-gray-600 text-sm sm:text-base">{service.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </section>
-   
   );
 };
 
