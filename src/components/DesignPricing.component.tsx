5a8aebeafce0533571f78ce76d43b65501728d60
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-17 15:02:18 +0300

    updated priceing styling

--- src/components/DesignPricing.component.tsx
+++ src/components/DesignPricing.component.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const DesignPricing: React.FC = () => {
   const [isPro, setIsPro] = useState(false);
-
+  const { t } = useTranslation();
+  
   const togglePlan = () => {
     setIsPro(!isPro);
   };
@@ -41,58 +43,55 @@ const DesignPricing: React.FC = () => {
   ];
 
   return (
-    <section className="p-10 sm:py-16 lg:py-24 tracking-wide">
-      <div className="container mx-auto py-16">
+    <section className="p-4 sm:p-10 sm:py-16 lg:py-24 tracking-wide bg-slate-100">
+      <div className="container mx-auto py-8 sm:py-16">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="max-w-2xl mx-auto md:text-center">
-            <h2 className="~text-4xl/6xl font-bold text-black md:text-center tracking-wide">
-              Pricing built for businesses of all sizes
+          <div className="max-w-2xl mx-auto text-center">
+            <h2 className="text-3xl sm:text-4xl font-bold text-black tracking-wide">
+              {t("Pricing built for businesses of all sizes")}
             </h2>
-            <p className="max-w-md mx-auto mt-4 text-xl leading-relaxed text-gray-600 py-4">
-              Simple, transparent pricing that grows with you. Pause or cancel
-              at anytime.
+            <p className="max-w-md mx-auto mt-4 text-sm sm:text-base leading-relaxed text-gray-600 py-4">
+              {t("Simple, transparent pricing that grows with you. Pause or cancel at anytime.")}
             </p>
           </div>
 
-          <div className="grid grid-cols-1 gap-20 mx-auto lg:grid-cols-2 mt-14 md:gap-4 justify-center">
+          <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 mt-8 sm:mt-14 md:gap-4 justify-center">
             {/* CTA Column */}
             <div className="bg-black rounded-xl shadow-lg border border-gray-200">
-              <div className="p-12">
-                <h3 className="~text-5xl/7xl font-semibold text-white font-spartan pt-2 w-full">
-                  Join
-                  <br /> design@atem
+              <div className="p-6 sm:p-12">
+                <h3 className="text-4xl sm:text-5xl font-semibold text-white font-spartan pt-2 w-full">
+                  {t("Join design@atem")}
+                  
                 </h3>
-                {/* <p className="mt-2.5 text-normal text-gray-600">
-                  Learn more about our services
-                </p> */}
 
                 <div>
                   <NavLink
                     to="https://calendly.com/louiscollinsjr/atem-intro"
                     title="Schedule a call with atem"
-                    className="inline-flex items-center px-4 py-3 mt-12 my-4 w-full text-white font-popins border border-gray-600 rounded-xl bg-black"
+                    className="inline-flex items-center px-4 py-3 mt-8 sm:mt-12 my-4 w-full text-white font-popins border border-gray-600 rounded-xl bg-black"
                   >
                     <div className="py-2">
-                      <p className="text-xl font-semibold pb-4">
-                        Book a 15-min intro call
+                      <p className="text-lg sm:text-xl font-semibold pb-2 sm:pb-4">
+                        {t("Book a 15-min intro call")}
                       </p>
-                      <p className="text-sm">
-                        Learn more about how design@atem works and how it can
-                        help you.
+                      <p className="text-xs sm:text-sm">
+                        {t("Learn more about how design@atem works and how it can help you.")}
                       </p>
                     </div>
                   </NavLink>
 
                   <NavLink
-                    to="https://calendly.com/louiscollinsjr/atem-intro"
+                    to="/contact"
                     title="Schedule a call with atem"
                     className="inline-flex items-center  px-4 py-3 w-full text-white font-popins border border-gray-600 rounded-xl bg-black"
                   >
                     <div className="py-2">
                       <p className="text-xl font-semibold pb-4">
-                        Refer a friend
+                        {t("Refer a friend")}
                       </p>
-                      <p className="text-sm">We appreciate your support!</p>
+                      <p className="text-sm">
+                        {t("We appreciate your support!")}
+                      </p>
                     </div>
                   </NavLink>
 
@@ -106,21 +105,20 @@ const DesignPricing: React.FC = () => {
                 key={plan.id}
                 className="overflow-hidden bg-white rounded-xl h-full shadow-md border border-gray-200"
               >
-                <div className="p-6 md:py-8 md:px-9">
-                  <h3 className="~text-3xl/5xl text-black pb-4">{plan.type}</h3>
-                  {/* <p className="mt-2.5 text-normal text-gray-600">{plan.description}</p> */}
+                <div className="p-4 sm:p-6 md:py-8 md:px-9">
+                  <h3 className="text-2xl sm:text-3xl text-black pb-4">{plan.type}</h3>
 
                   {plan.id === 'standard_pro' && (
-                    <div className="mt-4 flex items-center">
+                    <div className="mt-4 flex items-center justify-center sm:justify-start">
                       <div className="relative">
                         <div
-                          className={`w-72 h-12 flex items-center bg-gray-900 rounded-full p-2 duration-300 ease-in-out ${
+                          className={`w-64 sm:w-72 h-12 flex items-center bg-gray-900 rounded-full p-2 duration-300 ease-in-out ${
                             isPro ? 'bg-blue-600' : 'bg-gray-300'
                           }`}
                         >
                           <span
-                            className={`bg-yellow-400 w-36 h-10 rounded-full shadow-md transform duration-300 ease-in-out ${
-                              isPro ? 'translate-x-32' : ''
+                            className={`bg-yellow-400 w-28 sm:w-36 h-10 rounded-full shadow-md transform duration-300 ease-in-out ${
+                              isPro ? 'translate-x-[8rem] sm:translate-x-32' : ''
                             }`}
                           ></span>
                         </div>
@@ -133,14 +131,14 @@ const DesignPricing: React.FC = () => {
                               isPro ? 'text-white' : 'text-gray-700'
                             }`}
                           >
-                            Standard
+                            {t("Standard")}
                           </span>
                           <span
                             className={`flex-1 text-center ${
                               isPro ? 'text-gray-700' : 'text-white'
                             }`}
                           >
-                            🚀 Pro
+                            🚀 {t("Pro")}
                           </span>
                         </div>
                         <input
@@ -161,20 +159,20 @@ const DesignPricing: React.FC = () => {
                         </p>
 
                         <span className="ml-2 text-lg text-gray-600">
-                          {plan.onetime ? 'One time fee' : '/m'}
+                          {plan.onetime ? 'One time fee' : t("/m")}
                         </span>
                       </div>
                       <p className="text-sm text-gray-600 py-2">
-                        {plan.description}
+                        {t(plan.description)}
                       </p>
                     </div>
                   )}
 
                   {plan.items && (
                     <>
-                      <div className="h-[1px] bg-gray-300 my-6 w-full"></div>
-                      <p className="text-sm my-2">What's included</p>
-                      <ul className="grid grid-cols-2 gap-x-4 gap-y-4 mt-4">
+                      <div className="h-[1px] bg-gray-300 my-4 sm:my-6 w-full"></div>
+                      <p className="text-sm my-2">{t("What's included")}</p>
+                      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-2 sm:gap-y-4 mt-4">
                         {plan.items.map((item, itemIndex) => (
                           <li key={itemIndex} className="inline-flex space-x-1">
                             <svg
@@ -190,7 +188,7 @@ const DesignPricing: React.FC = () => {
                               />
                             </svg>
                             <span className="text-sm text-gray-900">
-                              {item}
+                              {t(item)}
                             </span>
                           </li>
                         ))}
@@ -198,25 +196,25 @@ const DesignPricing: React.FC = () => {
                     </>
                   )}
 
-                  <div className="flex items-center justify-start">
-                    <div className="">
+                  <div className="flex flex-col sm:flex-row items-center justify-start mt-6 sm:mt-0">
+                    <div className="w-full sm:w-auto">
                       <NavLink
                         to="https://calendly.com/louiscollinsjr/atem-intro"
                         title="Schedule a call with atem"
-                        className="inline-flex items-center justify-center px-4 py-3 my-12 text-white font-popins border border-transparent rounded-xl bg-black"
+                        className="inline-flex items-center justify-center px-4 py-3 my-4 sm:my-12 w-full sm:w-auto text-white font-popins border border-transparent rounded-xl bg-black"
                       >
-                        {plan.CTA || 'Get Started'}
+                        {t(plan.CTA || "Get Started")}
                       </NavLink>
                     </div>
-                    <div className="pl-4">
+                    <div className="pl-0 sm:pl-4 mt-2 sm:mt-0">
                       <p className="text-black text-sm">
-                        or{' '}
+                        {t("or")}{' '}
                         <NavLink
                           to="https://calendly.com/louiscollinsjr/atem-intro"
                           title="Schedule a call with atem"
                           className="underline"
                         >
-                          book a call
+                          {t("book a call")}
                         </NavLink>
                       </p>
                     </div>
