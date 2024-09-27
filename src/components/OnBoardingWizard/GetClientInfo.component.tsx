aa0f0902e44ffa7d8c2b9ec7457b1680c4e02f87
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-27 13:32:27 +0300

    updated styling for step 1

--- src/components/OnBoardingWizard/GetClientInfo.component.tsx
+++ src/components/OnBoardingWizard/GetClientInfo.component.tsx
@@ -46,42 +46,43 @@ const GetClientInfo: React.FC = () => {
   };
 
   return (
-    <section className="w-full mx-auto">
-      <div className="px-4 sm:px-6 lg:px-8 mb-16">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-14">
+    <section className="w-full mx-auto px-4 sm:px-8">
+      <div className="sm:px-6 lg:px-8 mb-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-x-14">
           <div className="@container mx-auto">
-            <div className='sm:pt-8'>
-              <h1 className="text-left text-4xl sm:text-5xl sm:w-[26rem] font-light  font-roboto tracking-normal">
-                Building together for{' '}
+            <div className="sm:pt-8">
+              <h1 className="text-left text-4xl sm:text-4xl md:text-6xl font-light font-roboto tracking-normal">
+                Building for{' '}
                 <span className="bg-gradient-to-r from-purple-800 via-blue-700 to-teal-500 bg-clip-text text-transparent">
-                  our community.
+                {' '}our community.
                 </span>
               </h1>
               <p className="text-left text-gray-700 font-light sm:w-[32rem] text-base pt-4">
                 We're thrilled to have you join our mission to support our
-                community with top-tier web design at an unbeatable price. This
-                special offer is all about empowering local businesses and
-                individuals to grow through thoughtful design and development.
-                Your participation helps us build something greater together.
+                community with top-tier web design at an unbeatable price.{' '}
+                <span className="hidden sm:inline">
+                  This special offer is all about empowering local businesses
+                  and individuals to grow through thoughtful design and
+                  development. Your participation helps us build something
+                  greater together.
+                </span>
               </p>
             </div>
           </div>
 
           <div className="container mx-auto">
-            <div className="text-left w-[32rem]">
+            <div className="text-left w-full sm:w-[32rem]">
               <div className="md:block sm:w-96">
                 <ProgressIndicator />
               </div>
               <p className="text-left text-gray-700 font-light sm:w-[32rem] text-base">
-                Let's get started on our design journey! Complete the onboarding
-                steps to share your goals and preferences, and together, we'll
-                create something amazing.
+                <b>Let's get started on our design journey!</b> Complete the onboarding steps to share your vision, goals, and preferences. Together, we’ll craft something truly amazing.
               </p>
               <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 gap-y-4 py-6">
                   <div>
                     <input
-                      className="p-2 border border-gray-300 w-full text-sm rounded-md"
+                      className="p-3 border border-gray-300 w-full text-sm rounded-md"
                       {...register('clientFirstName')}
                       placeholder="First name"
                     />
@@ -93,7 +94,7 @@ const GetClientInfo: React.FC = () => {
                   </div>
                   <div>
                     <input
-                      className="p-2 border border-gray-300 w-full text-sm rounded-md"
+                      className="p-3 border border-gray-300 w-full text-sm rounded-md"
                       {...register('clientLastName')}
                       placeholder="Last name"
                     />
@@ -105,7 +106,7 @@ const GetClientInfo: React.FC = () => {
                   </div>
                   <div className="md:col-span-2">
                     <input
-                      className="p-2 border border-gray-300 w-full text-sm rounded-md"
+                      className="p-3 border border-gray-300 w-full text-sm rounded-md"
                       {...register('clientEmail')}
                       placeholder="Email address"
                     />
@@ -117,7 +118,7 @@ const GetClientInfo: React.FC = () => {
                   </div>
                   <div className="sm:col-span-2 pt-4">
                     <button
-                      className="border px-4 py-2 rounded-md w-full text-base text-white bg-black font-popins tracking-wider"
+                      className="border px-4 py-3 rounded-md w-full text-base text-white bg-black font-popins tracking-wider"
                       type="submit"
                     >
                       Next
