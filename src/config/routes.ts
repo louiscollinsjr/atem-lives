60c7b3d4b6d503b3565936081b53a7c85cbb4e3e
louiscollinsjr <louiscollinsjr@gmail.com>
2024-09-30 19:45:51 +0300

    testing 404 routing

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -16,7 +16,7 @@ export interface RouteConfig {
   import Design349 from '../components/Design349GetStarted.component';
   import OnBoardingWizard from '../components/OnBoardingWizard/OnBoardingWizard.component';
   import HomeContactForm from '../components/HomeContactForm.component';
-  import NotFoundPage from '../components/NotFound.component';
+  import NotFound from '../components/not-found';
 
 
 
@@ -28,7 +28,7 @@ export interface RouteConfig {
     { path: '/design349', spotlightText: '349.Design. Spotlight', isDarkBackground: true,  component: Design349 },
     { path: '/onboarding', spotlightText: 'Local❤️349.Design', isDarkBackground: false,  component: OnBoardingWizard },
     { path: '/contact', spotlightText: 'Contact', isDarkBackground: false,  component: HomeContactForm },
-    { path: '*', spotlightText: '404 Page Not Found', isDarkBackground: false,  component: NotFoundPage},
+    { path: '*', spotlightText: '404 Page Not Found', isDarkBackground: false,  component: NotFound},
   ];
   
   export const defaultRoute: RouteConfig = {
--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [react()],
-  // Set base URL depending on mode
-  //base: mode === 'production' ? 'https://atem-lives.b-cdn.net/' : '/',
-  // build: {
-  //   outDir: 'dist',
-  //   rollupOptions: {
-  //     output: {
-  //       assetFileNames: 'assets/[name].[hash].[ext]',
-  //     },
-  //   },
-  // },
-  // define: {
-  //   'process.env': process.env
-  // }
-}));
+});
