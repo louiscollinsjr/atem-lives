484cbc296120f2e6de3888d63992751de47b0030
louiscollinsjr <louiscollinsjr@gmail.com>
2024-10-01 07:18:23 +0300

    updated  vite config on main - bad merge

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Set base URL depending on mode
-  //base: mode === 'production' ? 'https://atem-lives.b-cdn.net/' : '/',
-  // build: {
-  //   outDir: 'dist',
-  //   rollupOptions: {
-  //     output: {
-  //       assetFileNames: 'assets/[name].[hash].[ext]',
-  //     },
-  //   },
-  // },
-  // define: {
-  //   'process.env': process.env
-  // }
-}));
+});
